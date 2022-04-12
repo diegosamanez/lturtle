de7360dcc812f0fa016fd4e7ba6e1fb71bd2526c
Diego Samanez <diegosamanez@gmail.com>
2022-04-12 20:55:53 +0200

    fix autoload

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 // use Agregalel\Lturtle\Console\Generator;
 
+if (file_exists(__DIR__.'/../../autoload.php')) {
+    require __DIR__.'/../../autoload.php';
+} else {
+    require __DIR__.'/vendor/autoload.php';
+}
+
 require __DIR__.'/vendor/autoload.php';
 
 // switch($argv[1]){
